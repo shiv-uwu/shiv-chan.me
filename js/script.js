2bb5f34e6dbdd6f68baf8be7cb3b39e221e00878
Shivers <96980244+shiv-uwu@users.noreply.github.com>
2023-05-08 17:16:57 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-	// Sticky Navbar
+	// sticky Navbar
     let header = document.querySelector('header');
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
      
      
-    window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => { 
         header.classList.toggle('shadow', window.scrollY > 0);
     });
      
@@ -15,7 +15,7 @@
         navbar.classList.remove('active');
     }
      
-    // Dark Mode
+    // dark Mode
     let darkmode = document.querySelector('#darkmode');
      
     darkmode.onclick = () => {
@@ -40,5 +40,52 @@
       }
       
       document.querySelector(".bx.bxl-discord-alt").addEventListener("click", function() {
-        copyToClipboard("shivu#4743");
+        copyToClipboard("nullptr#4066");
       });
+
+  function validateEmail(email) { // this is a function to validate the email address
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  var form = document.querySelector(".contact-form"); // this is the form
+  var nameField = form.querySelector("input[name='name']"); // this is the name field
+  var emailField = form.querySelector("input[name='email']"); // this is the email field
+  var messageField = form.querySelector("textarea[name='message']"); // this is the message field
+  var submitButton = form.querySelector(".contact-button"); // this is the submit button
+
+  var nameError = form.querySelector("#name-error"); 
+  var emailError = form.querySelector("#email-error");
+  var messageError = form.querySelector("#message-error");
+
+  form.addEventListener("submit", function(event) { // this is the event listener for the form
+    var isValid = true;
+
+    if (nameField.value.trim() === "") { // this is the validation for the name field
+      nameError.innerHTML = "Please enter your name.";
+      isValid = false;
+    } else {
+      nameError.innerHTML = ""; // this is the error message for the name field
+    }
+
+    if (emailField.value.trim() === "") {
+      emailError.innerHTML = "Please enter your email address.";
+      isValid = false;
+    } else if (!validateEmail(emailField.value)) {
+      emailError.innerHTML = "Please enter a valid email address.";
+      isValid = false;
+    } else {
+      emailError.innerHTML = "";
+    }
+
+    if (messageField.value.trim() === "") {
+      messageError.innerHTML = "Please enter your message.";
+      isValid = false;
+    } else {
+      messageError.innerHTML = "";
+    }
+
+    if (!isValid) {
+      event.preventDefault();
+    }
+  });
